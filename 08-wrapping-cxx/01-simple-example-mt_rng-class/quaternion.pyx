244c7e0b4223831c975d62c5297f0d7331e66c92
Jorge Zavaleta <xyzjorgeabc@gmail.com>
2023-02-23 15:41:49 +0100

    Parte 2

--- 08-wrapping-cxx/01-simple-example-mt_rng-class/quaternion.pyx
+++ 08-wrapping-cxx/01-simple-example-mt_rng-class/quaternion.pyx
@@ -31,3 +31,19 @@ cdef class PyQuaternion:
     @property
     def z(self):
         return self._thisptr.z
+
+    @w.setter
+    def w(self, val):
+        self._thisptr.w = val
+        
+    @x.setter
+    def x(self, val):
+        self._thisptr.x = val
+        
+    @y.setter
+    def y(self, val):
+        self._thisptr.y = val
+
+    @z.setter
+    def z(self, val):
+        self._thisptr.z = val
